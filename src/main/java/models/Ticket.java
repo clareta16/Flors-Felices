9771151d3133aa8f5d734f543f4340126487d2d6
clareta16 <clara.pages16@gmail.com>
2024-07-24 10:45:53 +0200

    canvis metodes

--- src/main/java/models/Ticket.java
+++ src/main/java/models/Ticket.java
@@ -1,5 +1,11 @@
 package models;
 
+import connexio.MySqlConnexio;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -59,25 +65,65 @@ public class Ticket {
     }
 
     public void afegirProducte(Producte producte){
-    productesTicket.add(producte);
+        productesTicket.add(producte); //cal afegir-lo o només guardant-ho a guardarProducte a la db ja està?
+        guardarProducte(producte);
     }
 
-    public void imprimirTicket(){
-        System.out.println("Ticket de compra  " + id);
-        System.out.println(data + hora);
+    private void guardarProducte(Producte producte) {
+        String query = "INSERT INTO productes (ticket_id, nom, tipus, preu, caracteristiques) VALUES (?, ?)";
 
-        for (Producte producte : productesTicket) {
-            System.out.println(producte.caracteristiquesProducte());
+        try (Connection connect = MySqlConnexio.getInstance().getConnexio();
+             PreparedStatement statement = connect.prepareStatement(query)) {
+            statement.setInt(1, this.id);
+            statement.setDouble(2, producte.getPreu());
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("S'ha produit un error a l'intentar guardar el producte " + e.getMessage());
+        }
+    }
+
+    public void imprimirTicket(Ticket ticket){
+        System.out.println("Ticket de la compra " + ticket.getId());
+        System.out.println(ticket.getData() + ticket.getHora());
+
+        String query = "SELECT * FROM productes WHERE ticket_id = ?";
+
+        try (Connection connect = MySqlConnexio.getInstance().getConnexio();
+             PreparedStatement statement = connect.prepareStatement(query)) {
+             statement.setInt(1, this.id);
+            ResultSet resultat = statement.executeQuery();
+
+            while (resultat.next()) {
+                double preu = resultat.getDouble("preu");
+                String nom = resultat.getString("nom");
+                System.out.println("El producte " +nom + " té un preu de " + preu + " euros");
+            }
+        } catch (SQLException e) {
+            System.out.println("Hi ha hagut un error a l'imprimir el ticket " + e.getMessage());
         }
 
-        System.out.println("Preu final: " + calcularTotal() + " euros");
-
+        System.out.println("El preu final és de " + calcularTotal() + " euros");
     }
 
     public double calcularTotal(){
-        for (Producte producte : productesTicket) {
-           this.preuTotal += producte.getPreu();
+        double total = 0.0;
+
+        String query = "SELECT SUM(preu) AS total FROM productes WHERE ticket_id = ?";
+
+        try (Connection connect = MySqlConnexio.getInstance().getConnexio();
+             PreparedStatement statement = connect.prepareStatement(query)) {
+            statement.setInt(1, this.id);
+            ResultSet resultat = statement.executeQuery();
+
+            if (resultat.next()) { // el .next és per passar a la següent fila de resultats
+                total = resultat.getDouble("total");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al calcular el total " + e.getMessage());
         }
-        return preuTotal;
+        return total;
     }
+
 }
