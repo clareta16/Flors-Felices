ac9d4526b52d4ab3450ce86477be6712c1b65fe5
clareta16 <clara.pages16@gmail.com>
2024-07-25 10:46:03 +0200

    classe entrada feta

---
+++ src/main/java/eines/Entrada.java
@@ -0,0 +1,83 @@
+package eines;
+
+import models.Material;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Entrada {
+
+private static final Scanner entrada = new Scanner(System.in);
+private static final String MISS_ERR_INT = "Hi ha un error de format. Escriu un número enter";
+private static final String MISS_ERR_DOUBLE = "Hi ha un error de format. Escriu un número decimal";
+private static final String MISS_ERR_STRING_BUIDA = "Hi ha un error de format. Escriu una paraula";
+private static final String MISS_ERR_ENUM = "Hi ha un error de format. Escriu fusta o plàstic";
+
+
+
+public static int entradaInt(String pregunta) {
+    int resposta = 0;
+    boolean correcte = false;
+    while (!correcte) {
+            System.out.println(pregunta);
+            try {
+                resposta = entrada.nextInt();
+                correcte = true;
+        } catch (InputMismatchException e){
+                System.out.println(MISS_ERR_INT);
+        }
+    }
+    entrada.nextLine();
+    return resposta;
+}
+
+    public static double entradaDouble(String pregunta) {
+        double resposta = 0;
+        boolean correcte = false;
+        while (!correcte) {
+            System.out.println(pregunta);
+            try {
+                resposta = entrada.nextDouble();
+                correcte = true;
+            } catch (InputMismatchException e){
+                System.out.println(MISS_ERR_DOUBLE);
+            }
+        }
+        entrada.nextLine();
+        return resposta;
+    }
+
+    public static String entradaBuida(String pregunta) {
+        String resposta = "";
+        boolean correcte = false;
+        while (!correcte) {
+            System.out.println(pregunta);
+            try {
+                resposta = entrada.nextLine();
+                correcte = true;
+            } catch (InputMismatchException e){
+                System.out.println(MISS_ERR_STRING_BUIDA);
+            }
+        }
+        entrada.nextLine();
+        return resposta;
+    }
+
+    public static Material entradaEnum(String pregunta) {
+    String resposta = "";
+    Material materialTriat = null;
+        boolean correcte = false;
+        while (!correcte) {
+            System.out.println(pregunta);
+            try {
+                resposta = entrada.nextLine();
+                materialTriat = Material.valueOf(resposta.toUpperCase());
+                correcte = true;
+            } catch (InputMismatchException e){
+                System.out.println(MISS_ERR_ENUM);
+            }
+        }
+        entrada.nextLine();
+        return materialTriat;
+    }
+}
