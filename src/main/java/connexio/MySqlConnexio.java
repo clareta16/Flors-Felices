2d5d95be71af9aaa735639444be8e432ab45790d
Arnau <arnalvberita@gmail.com>
2024-07-25 11:43:05 +0200

    update

--- src/main/java/connexio/MySqlConnexio.java
+++ src/main/java/connexio/MySqlConnexio.java
@@ -225,7 +225,7 @@ public class MySqlConnexio {
             try (ResultSet generatedKeys = ticketStatement.getGeneratedKeys()) {
                 if (generatedKeys.next()) {
                     int ticketId = generatedKeys.getInt(1);
-                   // ticket.getId(ticketId);
+                     ticket.setId(ticketId);
 
                     // Inserir cada producte al ticket i retirar-lo del stock
                     for (String nomProducte : nomsProductes) {
