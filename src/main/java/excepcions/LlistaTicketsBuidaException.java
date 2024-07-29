b0f9972c7c4e2df69751be4ea7ea7acd5609bf0c
clareta16 <clara.pages16@gmail.com>
2024-07-29 12:17:44 +0200

    test2 excepcio

--- src/main/java/excepcions/LlistaTicketsBuidaException.java
+++ src/main/java/excepcions/LlistaTicketsBuidaException.java
@@ -4,4 +4,7 @@ public class LlistaTicketsBuidaException extends Exception {
     public LlistaTicketsBuidaException(String missatge) {
         super(missatge);
     }
+
+
+
 }
---
+++ src/test/java/excepcions/LlistaTicketsBuidaExceptionTest.java
@@ -0,0 +1,7 @@
+package excepcions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LlistaTicketsBuidaExceptionTest {
+
+}
